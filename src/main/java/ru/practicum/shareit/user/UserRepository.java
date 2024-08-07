b77f9c2cb4e890482ca52f191273c69cdc81d203
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-08-07 16:40:35 +0300

    feat: add UserRepository implementation

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -3,11 +3,12 @@ package ru.practicum.shareit.user;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository {
     User createUser(User user);
 
-    User getUserById(Long id);
+    Optional<User> findUserById(Long id);
 
     List<User> getAllUsers();
 
---
+++ src/main/java/ru/practicum/shareit/user/UserRepositoryImpl.java
@@ -0,0 +1,44 @@
+package ru.practicum.shareit.user;
+
+import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+    private final Map<Long, User> users = new HashMap<>();
+    private long currentMaxId = 0;
+
+    @Override
+    public User createUser(User user) {
+        currentMaxId++;
+        user.setId(currentMaxId);
+        users.put(currentMaxId, user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findUserById(Long id) {
+        return (users.containsKey(id)) ? Optional.ofNullable(users.get(id)) : Optional.empty();
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return users.values().stream().toList();
+    }
+
+    @Override
+    public User updateUser(User user) {
+        return users.put(user.getId(), user);
+    }
+
+    @Override
+    public boolean deleteUser(Long id) {
+        User user = users.remove(id);
+        return user != null;
+    }
+}
