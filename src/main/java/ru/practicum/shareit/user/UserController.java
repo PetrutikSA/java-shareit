b06ab4ee6b27889078f682d09c9b0ebface029dc
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-08-07 16:02:50 +0300

    feat: add UserController crud-base

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,44 @@
 package ru.practicum.shareit.user;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.user.dto.UserCreateDto;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.dto.UserUpdateDto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/users")
 public class UserController {
+
+    @PostMapping
+    public UserDto createUser(@RequestBody UserCreateDto userCreateDto) {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public UserDto getUserById(@PathVariable Long id) {
+        return null;
+    }
+
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        return null;
+    }
+
+    @PatchMapping("/{id}")
+    public UserDto updateUser(@PathVariable Long id, UserUpdateDto userUpdateDto) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+    }
 }
