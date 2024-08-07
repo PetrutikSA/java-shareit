eb1256a01c2dca71ac8d8c4789bc80c09029f306
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-08-07 16:12:38 +0300

    feat: add UserService implementation base

---
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package ru.practicum.shareit.user;
+
+import ru.practicum.shareit.user.dto.UserCreateDto;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.dto.UserUpdateDto;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDto createUser(UserCreateDto userCreateDto) {
+        return null;
+    }
+
+    @Override
+    public UserDto getUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public UserDto updateUser(Long id, UserUpdateDto userUpdateDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+    }
+}
