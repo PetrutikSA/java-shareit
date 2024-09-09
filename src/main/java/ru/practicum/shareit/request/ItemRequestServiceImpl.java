31ed56964e8e1dfec215c1a479e833f85f500c0f
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-09-09 17:14:01 +0300

    feat: add createItemRequest method

--- src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
@@ -4,18 +4,33 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.request.dto.ItemRequestCreateDto;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.dto.ItemRequestMapper;
 import ru.practicum.shareit.request.dto.ItemRequestUpdateDto;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.request.model.ItemRequestStatus;
+import ru.practicum.shareit.user.UserRepository;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.util.exception.NotFoundException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ItemRequestServiceImpl implements ItemRequestService {
     private final ItemRequestRepository itemRequestRepository;
+    private final UserRepository userRepository;
+    private final ItemRequestMapper itemRequestMapper;
 
     @Override
     public ItemRequestDto createItemRequest(Long userId, ItemRequestCreateDto itemRequestCreateDto) {
-        return null;
+        User requester = getUserFromRepository(userId);
+        ItemRequest itemRequest = itemRequestMapper.itemRequestCreateToItemRequest(itemRequestCreateDto);
+        itemRequest.setRequester(requester);
+        itemRequest.setStatus(ItemRequestStatus.ACTIVE);
+        itemRequest.setCreated(LocalDateTime.now());
+        itemRequest = itemRequestRepository.save(itemRequest);
+        return itemRequestMapper.itemRequestToItemRequestDto(itemRequest);
     }
 
     @Override
@@ -39,5 +54,10 @@ public class ItemRequestServiceImpl implements ItemRequestService {
     @Override
     public ItemRequestDto getItemRequestById(Long userId, Long itemRequestId) {
         return null;
+    }
+
+    private User getUserFromRepository(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(id, User.class));
     }
 }
