c085a74f570174481c1b12c0266cacf5c9a195c9
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-09-10 13:26:52 +0300

    feat: add user client/controller gateway

---
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -0,0 +1,56 @@
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.practicum.shareit.user.dto.UserCreateDto;
+import ru.practicum.shareit.user.dto.UserUpdateDto;
+
+@Controller
+@RequestMapping(path = "/users")
+@RequiredArgsConstructor
+@Slf4j
+@Validated
+public class UserController {
+    private final UserClient userClient;
+
+    @PostMapping
+    public ResponseEntity<Object> createUser(@RequestBody @Validated UserCreateDto userCreateDto) {
+        log.info("Creating user {}", userCreateDto);
+        return userClient.createUser(userCreateDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getUserById(@PathVariable Long id) {
+        log.info("Getting user with Id={}", id);
+        return userClient.getUserById(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getAllUsers() {
+        log.info("Getting all users");
+        return userClient.getAllUsers();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable Long id,
+                                             @RequestBody @Validated UserUpdateDto userUpdateDto) {
+        log.info("Updating user {}", userUpdateDto);
+        return userClient.updateUser(id, userUpdateDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        log.info("Deleting user with id = {}", id);
+        userClient.deleteUser(id);
+    }
+}
